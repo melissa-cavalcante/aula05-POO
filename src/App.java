d058567e292228474a071bff531b620d405ab2da
melissa-cavalcante <melissa.massimino@gmail.com>
2024-03-20 12:14:13 -0300

    meu terceiro commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ola kauan");
+        System.out.println("Ola kauan 20/03/2024");
     }
 }
